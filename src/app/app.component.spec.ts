02b09e3c3dd6240ee1184cab22bab5668993aa7f
gabagonzalez <gaba2801@gmail.com>
2019-08-15 14:20:45 -0400

    created dependencies, installation of firebase and mapbox

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'SCL009-Desafio-Maps'`, () => {
+  it(`should have as title 'Desafio-Maps!'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('SCL009-Desafio-Maps');
+    expect(app.title).toEqual('Desafio-Maps!');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to SCL009-Desafio-Maps!');
+    expect(compiled.querySelector('h1').textContent).toContain('Desafio-Maps!');
   });
 });
