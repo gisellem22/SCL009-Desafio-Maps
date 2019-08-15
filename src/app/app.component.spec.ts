1e1fe862954bae7ab05181fb0dfa80ffff3871a4
gabagonzalez <gaba2801@gmail.com>
2019-08-15 17:46:27 -0400

    creating map.component and printing map on screen

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,13 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { MapComponent } from './map/map.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        MapComponent
       ],
     }).compileComponents();
   }));
@@ -16,16 +18,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Desafio-Maps!'`, () => {
+  it(`should have as title 'Run-Maps!'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Desafio-Maps!');
+    expect(app.title).toEqual('Run-Maps');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Desafio-Maps!');
+    expect(compiled.querySelector('h1').textContent).toContain('Run-Maps');
   });
 });
